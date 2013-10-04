63ba0e5332c6e44560d859e672b3ac4225053c23
breaker27 <mail@uwe-freese.de>
2013-10-04 19:51:51 +0200

    Autodetect microcontroller type in flash script.

--- firmware/extra/flash_firmware.cmd
+++ firmware/extra/flash_firmware.cmd
@@ -3,11 +3,17 @@
 rem find files
 for %%F IN (*.hex) DO set SHC_HEX=%%F
 
+rem detect ATMega type by device name
+set SHC_PROC=m168
+if %SHC_HEX%==shc_basestation.hex set SHC_PROC=m328p
+if %SHC_HEX%==shc_dimmer.hex set SHC_PROC=m328p
+
 echo.
 echo Firmware: %SHC_HEX%
+echo ATMega:   %SHC_PROC%
 echo.
 echo Do you want to flash firmware now? (Abort with Ctrl+C)
 echo.
 pause
-avrdude -p m328p -U flash:w:%SHC_HEX%
+avrdude -p %SHC_PROC% -U flash:w:%SHC_HEX%
 pause
