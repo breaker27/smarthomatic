a95076d93b3968bc36644664a8b1ec6ccadf9167
rr2000 <sb1972@web.de>
2014-04-05 19:44:56 +0200

    fhem integration: support battery and version messages

--- host_software/fhem/37_SHC_TEMP.pm
+++ host_software/fhem/37_SHC_TEMP.pm
@@ -83,25 +83,25 @@ SHC_TEMP_Parse($$)
   my $name = $hash->{NAME};
   my ($id, $pktcnt, $msgtype, $msggroupid, $msgid, $msgdata);
   my ($tmp, $hum, $brt, $bat);  # temperature, humidity, brightness, battery
+  my ($vmajor, $vminor, $vpatch, $vhash); # version information
   
-  # OLD smarthomatic v0.3.0
-  # Sender ID=21;Packet Counter=24338;Command ID=10;Command Name=Temperature Sensor Status;Battery=80;Temperature=18.49;Humidity=56.91;Brightness=1
   # NEW smarthomatic v0.5.0
-  # Packet Data: SenderID=31;PacketCounter=677;MessageType=8;MessageGroupID=10;MessageID=1;MessageData=0960001f0001;Temperature=24.00;Humidity=0.0;Brightness=62;
-
-  #if ($msg =~ /^Packet Data: Sender ID=(\d*);Packet Counter=(\d*);MessageType=(\d*);MessageGroupID=(\d*);MessageID=(\d*);MessageData=([0-9a-f]*);Temperature=([0-9\.]*);Humidity=([0-9\.]*);Brightness=(\d*)/)
-
-  if ($msg =~ /^Packet Data: SenderID=(\d*);PacketCounter=(\d*);MessageType=(\d*);MessageGroupID=(\d*);MessageID=(\d*);MessageData=([0-9a-f]*);Temperature=([0-9\.\-]*);Humidity=([0-9\.]*);Brightness=([0-9\.]*);/)
-  {
+  # Data Message:
+  #   Packet Data: SenderID=31;PacketCounter=677;MessageType=8;MessageGroupID=10;MessageID=1;MessageData=0960001f0001;Temperature=24.00;Humidity=0.0;Brightness=62;
+  # Battery Status Message:
+  #   Packet Data: SenderID=31;PacketCounter=102;MessageType=8;MessageGroupID=0;MessageID=5;MessageData=c20000000004;Percentage=97;
+  # Version Status Message:
+  #   Packet Data: SenderID=31;PacketCounter=809;MessageType=8;MessageGroupID=0;MessageID=1;MessageData=00000000000000000000000000000000000000000000;Major=0;Minor=0;Patch=0;Hash=00000000;
+  
+  #if ($msg =~ /^Packet Data: SenderID=(\d*);PacketCounter=(\d*);MessageType=(\d*);MessageGroupID=(\d*);MessageID=(\d*);MessageData=([0-9a-f]*);Temperature=([0-9\.\-]*);Humidity=([0-9\.]*);Brightness=([0-9\.]*);/)
+  if ($msg =~ /^Packet Data: SenderID=(\d*);PacketCounter=(\d*);MessageType=(\d*);MessageGroupID=(\d*);MessageID=(\d*);MessageData=([0-9a-f]*)/) {
     $id = $1;
     $pktcnt = $2;
     $msgtype = $3;
     $msggroupid = $4;
     $msgid = $5;
     $msgdata = $6;
-    $tmp = $7;
-    $hum = $8;
-    $brt = $9;
+
   } else {
     Log3 $hash, 4, "SHC_TEMP  ($msg) data error";
     return "";
@@ -111,33 +111,54 @@ SHC_TEMP_Parse($$)
   my $rhash = $modules{SHC_TEMP}{defptr}{$raddr};
   my $rname = $rhash?$rhash->{NAME}:$raddr;
 
-   if( !$modules{SHC_TEMP}{defptr}{$raddr} ) {
+  if( !$modules{SHC_TEMP}{defptr}{$raddr} ) {
      Log3 $name, 3, "SHC_TEMP Unknown device $rname, please define it";
 
      return "UNDEFINED SHC_TEMP_$rname SHC_TEMP $raddr";
-   }
+  }
 
   my @list;
   push(@list, $rname);
-
   $rhash->{SHC_TEMP_lastRcv} = TimeNow();
 
   my $readonly = AttrVal($rname, "readonly", "0" );
 
   readingsBeginUpdate($rhash);
-  readingsBulkUpdate($rhash, "state", "T: $tmp  H: $hum  B:$brt");
   readingsBulkUpdate($rhash, "id", $id);
   readingsBulkUpdate($rhash, "pktcnt", $pktcnt);
   readingsBulkUpdate($rhash, "msgtype", $msgtype);
   readingsBulkUpdate($rhash, "msggroupid", $msggroupid);
   readingsBulkUpdate($rhash, "msgid", $msgid);
   readingsBulkUpdate($rhash, "msgdata", $msgdata);
-  readingsBulkUpdate($rhash, "battery", $brt);
-  readingsBulkUpdate($rhash, "temperature", $tmp);
-  readingsBulkUpdate($rhash, "humidity", $hum);
-  readingsBulkUpdate($rhash, "brightness", $brt);
-  readingsEndUpdate($rhash,1);    # Do triggers to update log file
 
+  if ($msg =~ /.*;Temperature=([0-9\.\-]*);Humidity=([0-9\.]*);Brightness=([0-9\.]*);/) {
+    $tmp = $1;
+    $hum = $2;
+    $brt = $3;
+
+    readingsBulkUpdate($rhash, "state", "T: $tmp  H: $hum  B:$brt");
+    readingsBulkUpdate($rhash, "temperature", $tmp);
+    readingsBulkUpdate($rhash, "humidity", $hum);
+    readingsBulkUpdate($rhash, "brightness", $brt);
+
+  } elsif ($msg =~ /.*;Percentage=(\d*);/) {
+    $bat = $1;
+
+    readingsBulkUpdate($rhash, "battery", $bat);
+  } elsif ($msg =~ /.*;Major=(\d*);Minor=(\d*);Patch=(\d*);Hash=([0-9a-zA-Z]*);/) {
+    $vmajor = $1;
+    $vminor = $2;
+    $vpatch = $3;
+    $vhash = $4;
+
+    readingsBulkUpdate($rhash, "version_major", $vmajor);
+    readingsBulkUpdate($rhash, "version_minor", $vminor);
+    readingsBulkUpdate($rhash, "version_patch", $vpatch);
+    readingsBulkUpdate($rhash, "version_hash", $vhash);
+  }
+
+
+  readingsEndUpdate($rhash,1);    # Do triggers to update log file
   return @list;
 }
 
