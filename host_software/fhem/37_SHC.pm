b6515c8e491517e5c8aa86046027ac786c1d123f
rr2000 <sb1972@web.de>
2014-05-01 21:10:30 +0200

    use perltidy to enforce coding style

--- host_software/fhem/37_SHC.pm
+++ host_software/fhem/37_SHC.pm
@@ -21,42 +21,39 @@ sub SHC_SimpleWrite(@);
 my $clientsSHC = ":SHC_Dev:BASE:xxx:";
 
 my %matchListSHC = (
-    "1:SHC_Dev" => "^Packet Data: SenderID=[1-9]|0[1-9]|[1-9][0-9]|[0-9][0-9][0-9]|[0-3][0-9][0-9][0-9]|40[0-8][0-9]|409[0-6]", #1-4096 with leading zeros
-    "2:xxx" => "^\\S+\\s+22",
-    "3:xxx" => "^\\S+\\s+11",
-    "4:xxx" => "^\\S+\\s+9 ",
+  "1:SHC_Dev" => "^Packet Data: SenderID=[1-9]|0[1-9]|[1-9][0-9]|[0-9][0-9][0-9]|[0-3][0-9][0-9][0-9]|40[0-8][0-9]|409[0-6]",    #1-4096 with leading zeros
+  "2:xxx"     => "^\\S+\\s+22",
+  "3:xxx"     => "^\\S+\\s+11",
+  "4:xxx"     => "^\\S+\\s+9 ",
 );
 
-sub
-SHC_Initialize($)
+sub SHC_Initialize($)
 {
   my ($hash) = @_;
 
   require "$attr{global}{modpath}/FHEM/DevIo.pm";
 
-# Provider
+  # Provider
   $hash->{ReadFn}  = "SHC_Read";
   $hash->{WriteFn} = "SHC_Write";
   $hash->{ReadyFn} = "SHC_Ready";
 
-# Normal devices
-  $hash->{DefFn}   = "SHC_Define";
-  $hash->{UndefFn} = "SHC_Undef";
-  $hash->{GetFn}   = "SHC_Get";
-  $hash->{SetFn}   = "SHC_Set";
+  # Normal devices
+  $hash->{DefFn}      = "SHC_Define";
+  $hash->{UndefFn}    = "SHC_Undef";
+  $hash->{GetFn}      = "SHC_Get";
+  $hash->{SetFn}      = "SHC_Set";
   $hash->{ShutdownFn} = "SHC_Shutdown";
 }
 
 #####################################
-sub
-SHC_Define($$)
+sub SHC_Define($$)
 {
   my ($hash, $def) = @_;
   my @a = split("[ \t][ \t]*", $def);
 
-  if(@a != 3) {
-    my $msg = "wrong syntax: define <name> SHC {devicename[\@baudrate] ".
-                        "| devicename\@directio}";
+  if (@a != 3) {
+    my $msg = "wrong syntax: define <name> SHC {devicename[\@baudrate] " . "| devicename\@directio}";
     Log3 undef, 2, $msg;
     return $msg;
   }
@@ -64,11 +61,11 @@ SHC_Define($$)
   DevIo_CloseDev($hash);
 
   my $name = $a[0];
-  my $dev = $a[2];
-  $dev .= "\@19200" if( $dev !~ m/\@/ );
+  my $dev  = $a[2];
+  $dev .= "\@19200" if ($dev !~ m/\@/);
 
-  $hash->{Clients} = $clientsSHC;
-  $hash->{MatchList} = \%matchListSHC;
+  $hash->{Clients}    = $clientsSHC;
+  $hash->{MatchList}  = \%matchListSHC;
   $hash->{DeviceName} = $dev;
 
   my $ret = DevIo_OpenDev($hash, 0, "SHC_DoInit");
@@ -76,21 +73,20 @@ SHC_Define($$)
 }
 
 #####################################
-sub
-SHC_Undef($$)
+sub SHC_Undef($$)
 {
   my ($hash, $arg) = @_;
   my $name = $hash->{NAME};
 
   foreach my $d (sort keys %defs) {
-    if(defined($defs{$d}) &&
-       defined($defs{$d}{IODev}) &&
-       $defs{$d}{IODev} == $hash)
-      {
-        my $lev = ($reread_active ? 4 : 2);
-        Log3 $name, $lev, "$name: deleting port for $d";
-        delete $defs{$d}{IODev};
-      }
+    if ( defined($defs{$d})
+      && defined($defs{$d}{IODev})
+      && $defs{$d}{IODev} == $hash)
+    {
+      my $lev = ($reread_active ? 4 : 2);
+      Log3 $name, $lev, "$name: deleting port for $d";
+      delete $defs{$d}{IODev};
+    }
   }
 
   SHC_Shutdown($hash);
@@ -99,78 +95,74 @@ SHC_Undef($$)
 }
 
 #####################################
-sub
-SHC_Shutdown($)
+sub SHC_Shutdown($)
 {
   my ($hash) = @_;
   return undef;
 }
 
 #####################################
-sub
-SHC_Set($@)
+sub SHC_Set($@)
 {
   # TODO: Not adapted to SHC, copy from 36_JeeLink.pm
   my ($hash, @a) = @_;
 
   my $name = shift @a;
-  my $cmd = shift @a;
-  my $arg = join("", @a);
+  my $cmd  = shift @a;
+  my $arg  = join("", @a);
 
   my $list = "raw:noArg";
-  return $list if( $cmd eq '?' );
+  return $list if ($cmd eq '?');
 
-  if($cmd eq "raw") {
+  if ($cmd eq "raw") {
+
     #return "\"set SHC $cmd\" needs exactly one parameter" if(@_ != 4);
     #return "Expecting a even length hex number" if((length($arg)&1) == 1 || $arg !~ m/^[\dA-F]{12,}$/ );
     Log3 $name, 4, "$name: set $name $cmd $arg";
     SHC_SimpleWrite($hash, $arg);
 
   } else {
-    return "Unknown argument $cmd, choose one of ".$list;
+    return "Unknown argument $cmd, choose one of " . $list;
   }
 
   return undef;
 }
 
 #####################################
-sub
-SHC_Get($@)
+sub SHC_Get($@)
 {
   # TODO: Not adapted to SHC, copy from 36_JeeLink.pm
-  my ($hash, $name, $cmd ) = @_;
+  my ($hash, $name, $cmd) = @_;
 
   my $list = "devices:noArg initSHC:noArg";
 
-  if( $cmd eq "devices" ) {
+  if ($cmd eq "devices") {
     SHC_SimpleWrite($hash, "l");
-  } elsif( $cmd eq "initSHC" ) {
+  } elsif ($cmd eq "initSHC") {
     SHC_SimpleWrite($hash, "0c");
     SHC_SimpleWrite($hash, "2c");
   } else {
-    return "Unknown argument $cmd, choose one of ".$list;
+    return "Unknown argument $cmd, choose one of " . $list;
   }
 
   return undef;
 }
 
-sub
-SHC_Clear($)
+sub SHC_Clear($)
 {
   my $hash = shift;
 
   # Clear the pipe
   $hash->{RA_Timeout} = 0.1;
-  for(;;) {
+  for (; ;) {
     my ($err, undef) = SHC_ReadAnswer($hash, "Clear", 0, undef);
-    last if($err && $err =~ m/^Timeout/);
+    last if ($err && $err =~ m/^Timeout/);
   }
   delete($hash->{RA_Timeout});
 }
 
 #####################################
-sub
-SHC_DoInit($)
+sub SHC_DoInit($)
 {
   # TODO: Not adapted to SHC, copy from 36_JeeLink.pm
   my $hash = shift;
@@ -191,8 +183,7 @@ SHC_DoInit($)
 #####################################
 # This is a direct read for commands like get
 # Anydata is used by read file to get the filesize
-sub
-SHC_ReadAnswer($$$$)
+sub SHC_ReadAnswer($$$$)
 {
   # TODO: Not adapted to SHC, copy from 36_JeeLink.pm
   my ($hash, $arg, $anydata, $regexp) = @_;
@@ -200,41 +191,41 @@ SHC_ReadAnswer($$$$)
   my $name = $hash->{NAME};
 
   return ("No FD", undef)
-        if(!$hash || ($^O !~ /Win/ && !defined($hash->{FD})));
+    if (!$hash || ($^O !~ /Win/ && !defined($hash->{FD})));
 
   my ($mpandata, $rin) = ("", '');
   my $buf;
-  my $to = 3;                                         # 3 seconds timeout
-  $to = $hash->{RA_Timeout} if($hash->{RA_Timeout});  # ...or less
-  for(;;) {
+  my $to = 3;    # 3 seconds timeout
+  $to = $hash->{RA_Timeout} if ($hash->{RA_Timeout});    # ...or less
+  for (; ;) {
 
-    if($^O =~ m/Win/ && $hash->{USBDev}) {
-      $hash->{USBDev}->read_const_time($to*1000); # set timeout (ms)
-      # Read anstatt input sonst funzt read_const_time nicht.
+    if ($^O =~ m/Win/ && $hash->{USBDev}) {
+      $hash->{USBDev}->read_const_time($to * 1000);      # set timeout (ms)
+                                                         # Read anstatt input sonst funzt read_const_time nicht.
       $buf = $hash->{USBDev}->read(999);
       return ("Timeout reading answer for get $arg", undef)
-        if(length($buf) == 0);
+        if (length($buf) == 0);
 
     } else {
       return ("Device lost when reading answer for get $arg", undef)
-        if(!$hash->{FD});
+        if (!$hash->{FD});
 
       vec($rin, $hash->{FD}, 1) = 1;
       my $nfound = select($rin, undef, undef, $to);
-      if($nfound < 0) {
+      if ($nfound < 0) {
         next if ($! == EAGAIN() || $! == EINTR() || $! == 0);
         my $err = $!;
         DevIo_Disconnected($hash);
-        return("SHC_ReadAnswer $arg: $err", undef);
+        return ("SHC_ReadAnswer $arg: $err", undef);
       }
       return ("Timeout reading answer for get $arg", undef)
-        if($nfound == 0);
+        if ($nfound == 0);
       $buf = DevIo_SimpleRead($hash);
-      return ("No data", undef) if(!defined($buf));
+      return ("No data", undef) if (!defined($buf));
 
     }
 
-    if($buf) {
+    if ($buf) {
       Log3 $hash->{NAME}, 5, "$name: SHC/RAW (ReadAnswer): $buf";
       $mpandata .= $buf;
     }
@@ -242,30 +233,31 @@ SHC_ReadAnswer($$$$)
     chop($mpandata);
     chop($mpandata);
 
-    return (undef, $mpandata)
+    return (
+      undef, $mpandata
+      );
   }
 
 }
 
 #####################################
 # Check if the 1% limit is reached and trigger notifies
-sub
-SHC_XmitLimitCheck($$)
+sub SHC_XmitLimitCheck($$)
 {
   # TODO: Not adapted to SHC, copy from 36_JeeLink.pm
-  my ($hash,$fn) = @_;
+  my ($hash, $fn) = @_;
   my $now = time();
 
-  if(!$hash->{XMIT_TIME}) {
+  if (!$hash->{XMIT_TIME}) {
     $hash->{XMIT_TIME}[0] = $now;
     $hash->{NR_CMD_LAST_H} = 1;
     return;
   }
 
-  my $nowM1h = $now-3600;
-  my @b = grep { $_ > $nowM1h } @{$hash->{XMIT_TIME}};
+  my $nowM1h = $now - 3600;
+  my @b = grep {$_ > $nowM1h} @{$hash->{XMIT_TIME}};
 
-  if(@b > 163) {          # 163 comes from fs20. todo: verify if correct for SHC modulation
+  if (@b > 163) {    # 163 comes from fs20. todo: verify if correct for SHC modulation
 
     my $name = $hash->{NAME};
     Log3 $name, 2, "$name: SHC TRANSMIT LIMIT EXCEEDED";
@@ -276,63 +268,61 @@ SHC_XmitLimitCheck($$)
     push(@b, $now);
 
   }
-  $hash->{XMIT_TIME} = \@b;
+  $hash->{XMIT_TIME}     = \@b;
   $hash->{NR_CMD_LAST_H} = int(@b);
 }
 
 #####################################
-sub
-SHC_Write($$)
+sub SHC_Write($$)
 {
   # TODO: Not adapted to SHC, copy from 36_JeeLink.pm
-  my ($hash,$msg) = @_;
+  my ($hash, $msg) = @_;
   my $name = $hash->{NAME};
 
   Log3 $name, 5, "$name: sending $msg";
 
   SHC_AddQueue($hash, $msg);
+
   #SHC_SimpleWrite($hash, $msg);
 }
 
-sub
-SHC_SendFromQueue($$)
+sub SHC_SendFromQueue($$)
 {
   # TODO: Not adapted to SHC, copy from 36_JeeLink.pm
   my ($hash, $bstring) = @_;
   my $name = $hash->{NAME};
-  my $to = 0.05;
+  my $to   = 0.05;
 
-  if($bstring ne "") {
+  if ($bstring ne "") {
     my $sp = AttrVal($name, "sendpool", undef);
-    if($sp) {   # Is one of the SHC-fellows sending data?
+    if ($sp) {    # Is one of the SHC-fellows sending data?
       my @fellows = split(",", $sp);
       foreach my $f (@fellows) {
-        if($f ne $name &&
-           $defs{$f} &&
-           $defs{$f}{QUEUE} &&
-           $defs{$f}{QUEUE}->[0] ne "")
-          {
-            unshift(@{$hash->{QUEUE}}, "");
-            InternalTimer(gettimeofday()+$to, "SHC_HandleWriteQueue", $hash, 1);
-            return;
-          }
+        if ( $f ne $name
+          && $defs{$f}
+          && $defs{$f}{QUEUE}
+          && $defs{$f}{QUEUE}->[0] ne "")
+        {
+          unshift(@{$hash->{QUEUE}}, "");
+          InternalTimer(gettimeofday() + $to, "SHC_HandleWriteQueue", $hash, 1);
+          return;
+        }
       }
     }
 
-    SHC_XmitLimitCheck($hash,$bstring);
+    SHC_XmitLimitCheck($hash, $bstring);
     SHC_SimpleWrite($hash, $bstring);
   }
 
-  InternalTimer(gettimeofday()+$to, "SHC_HandleWriteQueue", $hash, 1);
+  InternalTimer(gettimeofday() + $to, "SHC_HandleWriteQueue", $hash, 1);
 }
 
-sub
-SHC_AddQueue($$)
+sub SHC_AddQueue($$)
 {
   # TODO: Not adapted to SHC, copy from 36_JeeLink.pm
   my ($hash, $bstring) = @_;
-  if(!$hash->{QUEUE}) {
-    $hash->{QUEUE} = [ $bstring ];
+  if (!$hash->{QUEUE}) {
+    $hash->{QUEUE} = [$bstring];
     SHC_SendFromQueue($hash, $bstring);
 
   } else {
@@ -341,20 +331,19 @@ SHC_AddQueue($$)
 }
 
 #####################################
-sub
-SHC_HandleWriteQueue($)
+sub SHC_HandleWriteQueue($)
 {
   # TODO: Not adapted to SHC, copy from 36_JeeLink.pm
   my $hash = shift;
-  my $arr = $hash->{QUEUE};
-  if(defined($arr) && @{$arr} > 0) {
+  my $arr  = $hash->{QUEUE};
+  if (defined($arr) && @{$arr} > 0) {
     shift(@{$arr});
-    if(@{$arr} == 0) {
+    if (@{$arr} == 0) {
       delete($hash->{QUEUE});
       return;
     }
     my $bstring = $arr->[0];
-    if($bstring eq "") {
+    if ($bstring eq "") {
       SHC_HandleWriteQueue($hash);
     } else {
       SHC_SendFromQueue($hash, $bstring);
@@ -364,14 +353,13 @@ SHC_HandleWriteQueue($)
 
 #####################################
 # called from the global loop, when the select for hash->{FD} reports data
-sub
-SHC_Read($)
+sub SHC_Read($)
 {
   # TODO: Not adapted to SHC, copy from 36_JeeLink.pm
   my ($hash) = @_;
 
   my $buf = DevIo_SimpleRead($hash);
-  return "" if(!defined($buf));
+  return "" if (!defined($buf));
 
   my $name = $hash->{NAME};
 
@@ -379,47 +367,49 @@ SHC_Read($)
   Log3 $name, 5, "$name: SHC/RAW: $pandata/$buf";
   $pandata .= $buf;
 
-  while($pandata =~ m/\n/) {
+  while ($pandata =~ m/\n/) {
     my $rmsg;
-    ($rmsg,$pandata) = split("\n", $pandata, 2);
+    ($rmsg, $pandata) = split("\n", $pandata, 2);
     $rmsg =~ s/\r//;
-    SHC_Parse($hash, $hash, $name, $rmsg) if($rmsg);
+    SHC_Parse($hash, $hash, $name, $rmsg) if ($rmsg);
   }
   $hash->{PARTIAL} = $pandata;
 }
 
-sub
-SHC_Parse($$$$)
+sub SHC_Parse($$$$)
 {
   my ($hash, $iohash, $name, $rmsg) = @_;
   my $dmsg = $rmsg;
 
-  next if(!$dmsg || length($dmsg) < 1);                 # Bogus messages
+  next if (!$dmsg || length($dmsg) < 1);    # Bogus messages
 
-  if($dmsg !~ m/^Packet Data: SenderID=/) {
+  if ($dmsg !~ m/^Packet Data: SenderID=/) {
 
     # Messages just to dipose
-    if($dmsg =~ m/^\*\*\* Enter AES key nr/  ||
-        $dmsg =~ m/^\*\*\* Received character/) {
+    if ( $dmsg =~ m/^\*\*\* Enter AES key nr/
+      || $dmsg =~ m/^\*\*\* Received character/)
+    {
       return;
     }
 
     # -Verbosity level 5
-    if($dmsg =~ m/^Received \(AES key/  ||
-        $dmsg =~ m/^Received garbage/ ||
-        $dmsg =~ m/^Before encryption/ ||
-        $dmsg =~ m/^After encryption/ ||
-        $dmsg =~ m/^Repeating request./ ||
-        $dmsg =~ m/^Request Queue empty/ ||
-        $dmsg =~ m/^Removing request from request buffer/ ) {
+    if ( $dmsg =~ m/^Received \(AES key/
+      || $dmsg =~ m/^Received garbage/
+      || $dmsg =~ m/^Before encryption/
+      || $dmsg =~ m/^After encryption/
+      || $dmsg =~ m/^Repeating request./
+      || $dmsg =~ m/^Request Queue empty/
+      || $dmsg =~ m/^Removing request from request buffer/)
+    {
       Log3 $name, 5, "$name: $dmsg";
       return;
     }
 
     # -Verbosity level 4
-    if($dmsg =~ m/^Request added to queue/  ||
-        $dmsg =~ m/^Request Buffer/ ||
-        $dmsg =~ m/^Request (q|Q)ueue/ ) {
+    if ( $dmsg =~ m/^Request added to queue/
+      || $dmsg =~ m/^Request Buffer/
+      || $dmsg =~ m/^Request (q|Q)ueue/)
+    {
       Log3 $name, 4, "$name: $dmsg";
       return;
     }
@@ -437,47 +427,43 @@ SHC_Parse($$$$)
   Dispatch($hash, $dmsg, \%addvals);
 }
 
-
 #####################################
-sub
-SHC_Ready($)
+sub SHC_Ready($)
 {
   # TODO: Not adapted to SHC, copy from 36_JeeLink.pm
   my ($hash) = @_;
 
   return DevIo_OpenDev($hash, 1, "SHC_DoInit")
-                if($hash->{STATE} eq "disconnected");
+    if ($hash->{STATE} eq "disconnected");
 
   # This is relevant for windows/USB only
   my $po = $hash->{USBDev};
   my ($BlockingFlags, $InBytes, $OutBytes, $ErrorFlags);
-  if($po) {
+  if ($po) {
     ($BlockingFlags, $InBytes, $OutBytes, $ErrorFlags) = $po->status;
   }
-  return ($InBytes && $InBytes>0);
+  return ($InBytes && $InBytes > 0);
 }
 
 ########################
-sub
-SHC_SimpleWrite(@)
+sub SHC_SimpleWrite(@)
 {
   my ($hash, $msg) = @_;
-  return if(!$hash);
+  return if (!$hash);
 
   my $name = $hash->{NAME};
   Log3 $name, 5, "$name: SW: $msg";
 
   $msg .= "\r";
 
-  $hash->{USBDev}->write($msg)    if($hash->{USBDev});
-  syswrite($hash->{DIODev}, $msg) if($hash->{DIODev});
+  $hash->{USBDev}->write($msg) if ($hash->{USBDev});
+  syswrite($hash->{DIODev}, $msg) if ($hash->{DIODev});
 
   # Some linux installations are broken with 0.001, T01 returns no answer
   select(undef, undef, undef, 0.01);
 }
 
-sub
-SHC_Attr(@)
+sub SHC_Attr(@)
 {
   my @a = @_;
 
