9bd0c3e9e4644275fcddc5be9b8a521d5356684e
breaker27 <mail@uwe-freese.de>
2015-02-15 11:48:43 +0100

    Change condition format for toggle. Correct DevStateIcon so toggling works by clicking the icon.

--- host_software/fhem/37_SHCdev.pm
+++ host_software/fhem/37_SHCdev.pm
@@ -33,7 +33,7 @@ use SHC_parser;
 my $parser = new SHC_parser();
 
 my %dev_state_icons = (
-  "PowerSwitch"         => "on:on:toggle off:off:toggle set.*:light_question:off",
+  "PowerSwitch"         => ".*10000000:on:off .*00000000:off:on set.*:light_question:off",
   "Dimmer"              => "on:on off:off set.*:light_question:off",
   "EnvSensor"           => undef,
   "RGBDimmer"           => undef,
@@ -456,7 +456,7 @@ sub SHCdev_Set($@)
       # is able to do this as well. Even more it supports intervals, off-for-timer, off-till ...
 
       if ($cmd eq 'toggle') {
-        $cmd = ReadingsVal($name, "state", "on") ~~ m/......0......./ ? "on" : "off";
+		$cmd = ReadingsVal($name, "on", "0") eq "0" ? "on" : "off";
       }
 
       if (!$readonly && $cmd eq 'off') {
