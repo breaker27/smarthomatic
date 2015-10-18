a736cde76a19d1a99fd732814ac8e5f43b223a21
breaker27 <mail@uwe-freese.de>
2015-10-18 12:05:44 +0200

    Initial files for clock device.

---
+++ firmware/shc_clock/fuses.c
@@ -0,0 +1,29 @@
+/*
+* This file is part of smarthomatic, http://www.smarthomatic.org.
+* Copyright (c) 2015 Uwe Freese
+*
+* smarthomatic is free software: you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the
+* Free Software Foundation, either version 3 of the License, or (at your
+* option) any later version.
+*
+* smarthomatic is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+* Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with smarthomatic. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <avr/io.h> 
+
+FUSES = 
+{ 
+	// value 0xE2 - disable clock divider for 8 MHz clock, the other fuses are default for ATMega328
+	.low = (FUSE_CKSEL0 & FUSE_CKSEL2 & FUSE_CKSEL3 & FUSE_SUT0),
+	// value 0xD1 - EESAVE is 0, others are default
+	.high = (FUSE_SPIEN & FUSE_EESAVE & FUSE_BOOTSZ1 & FUSE_BOOTSZ0),
+	// value 0xFD - set BOD to 2.7V to prevent accidentially FLASH garbage
+	.extended = FUSE_BODLEVEL1,
+};
---
+++ firmware/shc_clock/shc_clock.c
@@ -0,0 +1,397 @@
+/*
+* This file is part of smarthomatic, http://www.smarthomatic.org.
+* Copyright (c) 2015 Uwe Freese
+*
+* smarthomatic is free software: you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the
+* Free Software Foundation, either version 3 of the License, or (at your
+* option) any later version.
+*
+* smarthomatic is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+* Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with smarthomatic. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <string.h>
+
+#include "rfm12.h"
+#include "../src_common/uart.h"
+
+#include "../src_common/msggrp_generic.h"
+#include "../src_common/msggrp_gpio.h"
+
+#include "../src_common/e2p_hardware.h"
+#include "../src_common/e2p_generic.h"
+
+#include "../src_common/aes256.h"
+#include "../src_common/util.h"
+#include "version.h"
+
+#define SEND_STATUS_EVERY_SEC 1800 // how often should a status be sent?
+#define SEND_VERSION_STATUS_CYCLE 50 // send version status x times less than switch status (~once per day)
+
+#define DCF_PINREG PINC
+#define DCF_PORTREG PORTC
+#define DCF_PIN 5
+
+
+uint16_t device_id;
+uint32_t station_packetcounter;
+
+uint16_t send_status_timeout = 5;
+uint8_t version_status_cycle = SEND_VERSION_STATUS_CYCLE - 1; // send promptly after startup
+
+void send_deviceinfo_status(void)
+{
+	inc_packetcounter();
+
+	UART_PUTF("Send DeviceInfo: DeviceType %u,", DEVICETYPE_POWERSWITCH);
+	UART_PUTF4(" v%u.%u.%u (%08lx)\r\n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, VERSION_HASH);
+	
+	// Set packet content
+	pkg_header_init_generic_deviceinfo_status();
+	pkg_header_set_senderid(device_id);
+	pkg_header_set_packetcounter(packetcounter);
+	msg_generic_deviceinfo_set_devicetype(DEVICETYPE_POWERSWITCH);
+	msg_generic_deviceinfo_set_versionmajor(VERSION_MAJOR);
+	msg_generic_deviceinfo_set_versionminor(VERSION_MINOR);
+	msg_generic_deviceinfo_set_versionpatch(VERSION_PATCH);
+	msg_generic_deviceinfo_set_versionhash(VERSION_HASH);
+
+	rfm12_send_bufx();
+}
+
+void send_time_currenttime_status(void)
+{
+	// TODO: Send current time status.
+}
+
+void process_gpio_digitalpintimeout(MessageTypeEnum messagetype)
+{
+	// TODO: Process received temperature
+}
+
+// Process a request to this device.
+// React accordingly on the MessageType, MessageGroup and MessageID
+// and send an Ack in any case. It may be an error ack if request is not supported.
+void process_request(MessageTypeEnum messagetype, uint32_t messagegroupid, uint32_t messageid)
+{
+	// remember some values before the packet buffer is destroyed
+/*	uint32_t acksenderid = pkg_header_get_senderid();
+	uint32_t ackpacketcounter = pkg_header_get_packetcounter();
+	
+	UART_PUTF("MessageGroupID:%u;", messagegroupid);
+	
+	if (messagegroupid != MESSAGEGROUP_GPIO)
+	{
+		UART_PUTS("\r\nERR: Unsupported MessageGroupID.\r\n");
+		send_ack(acksenderid, ackpacketcounter, true);
+		return;
+	}
+	
+	UART_PUTF("MessageID:%u;", messageid);
+
+	switch (messageid)
+	{
+		case MESSAGEID_GPIO_DIGITALPORT:
+			process_gpio_digitalport(messagetype);
+			break;
+		case MESSAGEID_GPIO_DIGITALPIN:
+			process_gpio_digitalpin(messagetype);
+			break;
+		case MESSAGEID_GPIO_DIGITALPORTTIMEOUT:
+			process_gpio_digitalporttimeout(messagetype);
+			break;
+		case MESSAGEID_GPIO_DIGITALPINTIMEOUT:
+			process_gpio_digitalpintimeout(messagetype);
+			break;
+		default:
+			UART_PUTS("\r\nERR: Unsupported MessageID.");
+			send_ack(acksenderid, ackpacketcounter, true);
+			return;
+	}
+	
+	UART_PUTS("\r\n");
+
+	// In all cases, use the digitalporttimer message as answer.
+	// It contains the data for *all* pins and *all* timer values.
+
+	// "Set" -> send "Ack"
+	if (messagetype == MESSAGETYPE_SET)
+	{
+		pkg_header_init_gpio_digitalporttimeout_ack();
+
+		UART_PUTS("Sending Ack\r\n");
+	}
+	// "Get" or "SetGet" -> send "AckStatus"
+	else
+	{
+		pkg_header_init_gpio_digitalporttimeout_ackstatus();
+		
+		uint8_t i;
+		
+		// react on changed state (version for more than one switch...)
+		for (i = 0; i < SWITCH_COUNT; i++)
+		{
+			// set message data
+			msg_gpio_digitalporttimeout_set_on(i, switch_state[i]);
+			msg_gpio_digitalporttimeout_set_timeoutsec(i, switch_timeout[i]);
+		}
+		
+		UART_PUTS("Sending AckStatus\r\n");
+	}
+
+	send_ack(acksenderid, ackpacketcounter, false);
+	send_status_timeout = 5; */
+}
+
+// Check if incoming message is a legitimate request for this device.
+// If not, ignore it.
+void process_packet(uint8_t len)
+{
+	pkg_header_adjust_offset();
+
+	// check SenderID
+	/*
+	uint32_t senderID = pkg_header_get_senderid();
+	UART_PUTF("Packet Data: SenderID:%u;", senderID);
+	
+	if (senderID != 0)
+	{
+		UART_PUTS("\r\nERR: Illegal SenderID.\r\n");
+		return;
+	}
+
+	// check PacketCounter
+	// TODO: Reject if packet counter lower than remembered!!
+	uint32_t packcnt = pkg_header_get_packetcounter();
+	UART_PUTF("PacketCounter:%lu;", packcnt);
+
+	if (0) // packcnt <= station_packetcounter ??
+	{
+		UART_PUTF("\r\nERR: Received PacketCounter < %lu.\r\n", station_packetcounter);
+		return;
+	}
+	
+	// write received counter
+	station_packetcounter = packcnt;
+	
+	e2p_powerswitch_set_basestationpacketcounter(station_packetcounter);
+	
+	// check MessageType
+	MessageTypeEnum messagetype = pkg_header_get_messagetype();
+	UART_PUTF("MessageType:%u;", messagetype);
+	
+	if ((messagetype != MESSAGETYPE_GET) && (messagetype != MESSAGETYPE_SET) && (messagetype != MESSAGETYPE_SETGET))
+	{
+		UART_PUTS("\r\nERR: Unsupported MessageType.\r\n");
+		return;
+	}
+	
+	// check device id
+	uint16_t rcv_id = pkg_headerext_common_get_receiverid();
+
+	UART_PUTF("ReceiverID:%u;", rcv_id);
+	
+	if (rcv_id != device_id)
+	{
+		UART_PUTS("\r\nWRN: DeviceID does not match.\r\n");
+		return;
+	}
+	
+	// check MessageGroup + MessageID
+	uint32_t messagegroupid = pkg_headerext_common_get_messagegroupid();
+	uint32_t messageid = pkg_headerext_common_get_messageid();
+	
+	process_request(messagetype, messagegroupid, messageid);*/
+}
+
+// Called every 10ms.
+ISR (TIMER0_COMPA_vect)
+{
+	UART_PUTS(".");
+}
+
+// Timer0 is used to detect length of DFC pulses.
+// 1 clock pulse = 1 ms
+// -> set overflow to 10
+void timer0_init(void)
+{
+	// Clock source = I/O clock, 1/1024 prescaler
+	// -> 1000 Hz @ 1 MHz CPU clock
+	TCCR0B = (1 << CS02) | (1 << CS00);
+
+	// Clear timer on compare match
+	TCCR0A = (1 << WGM01);
+
+	// set compare match value to 10 = 10 ms @ 1 MHz CPU clock
+	OCR0A = 100;
+
+	// Timer/Counter0 Output Compare Match A Interrupt Enable
+	TIMSK0 = (1 << OCIE0A);
+}
+
+int main(void)
+{
+	uint8_t loop = 0;
+	uint8_t i = 0;
+	uint8_t j = 0;
+	uint8_t state = 0;
+	uint8_t last_state = 0;
+
+	// delay 1s to avoid further communication with uart or RFM12 when my programmer resets the MC after 500ms...
+	_delay_ms(1000);
+
+	util_init();
+	
+	//check_eeprom_compatibility(DEVICETYPE_POWERSWITCH);
+
+	// read packetcounter, increase by cycle and write back
+	packetcounter = e2p_generic_get_packetcounter() + PACKET_COUNTER_WRITE_CYCLE;
+	e2p_generic_set_packetcounter(packetcounter);
+
+	// read device id
+	device_id = e2p_generic_get_deviceid();
+
+	osccal_init();
+
+	uart_init();
+
+	UART_PUTS ("\r\n");
+	UART_PUTF4("smarthomatic Clock v%u.%u.%u (%08lx)\r\n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, VERSION_HASH);
+	UART_PUTS("(c) 2015 Uwe Freese, www.smarthomatic.org\r\n");
+	osccal_info();
+	UART_PUTF ("DeviceID: %u\r\n", device_id);
+	UART_PUTF ("PacketCounter: %lu\r\n", packetcounter);
+	
+	// init AES key
+	e2p_generic_get_aeskey(aes_key);
+
+	led_blink(500, 500, 3);
+
+//	rfm_watchdog_init(device_id, e2p_clock_get_transceiverwatchdogtimeout());
+	rfm12_init();
+
+	sei();
+
+	// set pullup for DCF receiver
+	sbi(DCF_PORTREG, DCF_PIN);
+
+	timer0_init();
+	
+	while (1)
+	{
+		_delay_ms(1000);
+	}
+
+	while (1)
+	{
+		state = (DCF_PINREG >> DCF_PIN) & 1;
+		
+		switch_led(state);
+		
+		if (state)
+		{
+			j++;
+		}
+		
+		if ((last_state == 1) && (state == 0))
+		{
+			j = 0;
+		}
+		else if ((last_state == 0) && (state == 1))
+		{
+			i++;
+			UART_PUTF2("%03d %d ms\r\n", i, j);
+		}
+		
+		last_state = state;
+		
+	
+		_delay_ms(1);		
+	}
+
+	while (42)
+	{
+		if (rfm12_rx_status() == STATUS_COMPLETE)
+		{
+			uint8_t len = rfm12_rx_len();
+			
+			//rfm_watchdog_reset();
+			
+			if ((len == 0) || (len % 16 != 0))
+			{
+				UART_PUTF("Received garbage (%u bytes not multiple of 16): ", len);
+				print_bytearray(bufx, len);
+			}
+			else // try to decrypt with all keys stored in EEPROM
+			{
+				memcpy(bufx, rfm12_rx_buffer(), len);
+				
+				//UART_PUTS("Before decryption: ");
+				//print_bytearray(bufx, len);
+					
+				aes256_decrypt_cbc(bufx, len);
+
+				//UART_PUTS("Decrypted bytes: ");
+				//print_bytearray(bufx, len);
+
+				if (!pkg_header_check_crc32(len))
+				{
+					UART_PUTS("Received garbage (CRC wrong after decryption).\r\n");
+				}
+				else
+				{
+					process_packet(len);
+				}
+			}
+
+			// tell the implementation that the buffer can be reused for the next data.
+			rfm12_rx_clear();
+		}
+
+		// flash LED every second to show the device is alive
+		if (loop == 50)
+		{
+			loop = 0;
+			
+			// send status from time to time
+			send_status_timeout--;
+		
+			if (send_status_timeout == 0)
+			{
+				send_status_timeout = SEND_STATUS_EVERY_SEC;
+				send_time_currenttime_status();
+				led_blink(200, 0, 1);
+				
+				version_status_cycle++;
+			}
+			else if (version_status_cycle >= SEND_VERSION_STATUS_CYCLE)
+			{
+				version_status_cycle = 0;
+				send_deviceinfo_status();
+				led_blink(200, 0, 1);
+			}
+		}
+		else
+		{
+			_delay_ms(20);
+		}
+
+		rfm_watchdog_count(20);
+
+		rfm12_tick();
+
+		loop++;
+	}
+	
+	// never called
+	// aes256_done(&aes_ctx);
+}
