699716fe618d35457db7707015b05c3ce1202c1b
breaker27 <mail@uwe-freese.de>
2016-04-10 11:38:55 +0200

    Add source code for tea maker.

---
+++ firmware/shc_tea_maker/lcd.c
@@ -0,0 +1,195 @@
+/*
+* This file is part of smarthomatic, http://www.smarthomatic.org.
+* Copyright (c) 2016 Uwe Freese
+*
+* smarthomatic is free software: you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the
+* Free Software Foundation, either version 3 of the License, or (at your
+* option) any later version.
+*
+* smarthomatic is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+* Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with smarthomatic. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "lcd.h"
+#include <util/delay.h>
+#include <avr/io.h>
+#include <stdio.h>
+#include <avr/pgmspace.h>
+
+// This buffer is used for sending strings to the LCD.
+char lcdbuf[128];
+
+/* Anschluss des Displays über 4bit-Interface
+DB4-DB7       PC0-PC3
+RS            PC4
+E             PC5
+*/
+
+/* User Characters are stored in EEPROM at pos. 0-6
+
+// Ohm Character
+
+0b00000000 = 0x00
+0b00001110 = 0x0E
+0b00010001 = 0x11
+0b00010001 = 0x11
+0b00010001 = 0x11
+0b00001010 = 0x0A
+0b00011011 = 0x1B
+0b00000000 = 0x00
+
+*/
+
+#define LCD_SET_FUNCTION        0x20
+ 
+#define LCD_FUNCTION_4BIT       0x00
+#define LCD_FUNCTION_2LINE      0x08
+#define LCD_FUNCTION_5X7        0x00
+
+#define LCD_SET_ENTRY           0x04
+#define LCD_ENTRY_INCREASE      0x02
+#define LCD_ENTRY_NOSHIFT       0x00
+
+#define LCD_SET_DISPLAY         0x08
+ 
+#define LCD_DISPLAY_OFF         0x00
+#define LCD_DISPLAY_ON          0x04
+#define LCD_CURSOR_OFF          0x00
+#define LCD_CURSOR_ON           0x02
+#define LCD_BLINKING_OFF        0x00
+#define LCD_BLINKING_ON         0x01
+
+#define LCD_DDRAM 7
+#define LCD_START_LINE1 0x00
+#define LCD_START_LINE2 0x40
+
+#define LCD_PORT PORTA
+#define LCD_DDR DDRA
+#define LCD_PIN_EN	1
+#define LCD_PIN_RS  0
+
+void lcd_enable(void)
+{
+    LCD_PORT |= (1 << LCD_PIN_EN);
+    _delay_us(20);
+    LCD_PORT &= ~(1 << LCD_PIN_EN);
+}
+
+void lcd_out(uint8_t data)
+{
+    data &= 0xF0; // select upper 4 bits
+    LCD_PORT &= ~(0xF0 >> 2); // set 4 bits for D4..D7
+    LCD_PORT |= (data >> 2);
+    lcd_enable();
+}
+
+void lcd_data(unsigned char c)
+{
+	LCD_PORT |= (1 << LCD_PIN_RS); // RS == 1 (write data byte)
+ 
+    lcd_out(c);
+    lcd_out(c << 4);
+ 
+    _delay_us(50);
+}
+
+void lcd_command(unsigned char c)
+{
+	LCD_PORT &= ~(1 << LCD_PIN_RS); // RS == 0 (write command byte)
+ 
+    lcd_out(c);
+    lcd_out(c << 4);
+ 
+    _delay_us(50);
+}
+
+void lcd_init(void)
+{
+	unsigned char c;
+	unsigned int i;
+	
+	// set some I/O pins for LCD to output mode
+	LCD_DDR = 0b00111111;
+
+	_delay_ms(20); // startup delay
+	
+	// muss 3mal hintereinander gesendet werden zur Initialisierung
+	lcd_out(0x30);
+	_delay_ms(5);
+	lcd_enable();
+	_delay_ms(5);
+	lcd_enable();
+	_delay_ms(5);
+
+	// set 4 bit mode
+	lcd_out(LCD_SET_FUNCTION | LCD_FUNCTION_4BIT);
+	_delay_ms(5);
+
+    // set display format
+    lcd_command(LCD_SET_FUNCTION | LCD_FUNCTION_4BIT | LCD_FUNCTION_2LINE | LCD_FUNCTION_5X7);
+ 
+    // configure cursor
+    lcd_command(LCD_SET_DISPLAY | LCD_DISPLAY_ON | LCD_CURSOR_OFF | LCD_BLINKING_OFF); 
+ 
+    // configure cursor movement
+    lcd_command(LCD_SET_ENTRY | LCD_ENTRY_INCREASE | LCD_ENTRY_NOSHIFT );
+ 
+	lcd_clear();
+
+	// read user characters from eeprom and write to lcd
+	/*lcd_command((1<<3) | 0b01000000);
+	sleep(5);
+	
+	for (i = 0; i < 8 * 7; i++)
+	{
+		c = eeprom_read_uchar(i);
+
+	//sprintf(lcdbuf, "CHAR %u is %u\n", i, c);
+	//uart_puts(lcdbuf);
+
+		lcd_data(c);
+		sleep(5);
+	}
+	*/
+}
+
+void lcd_clear(void)
+{
+	lcd_command(0b00000001);
+    _delay_ms(5);
+}
+
+void lcd_putc(char c)
+{
+	lcd_data(c);	
+}
+
+void lcd_putstr(char * str)
+{
+	while(*str) lcd_data(*str++); // using lcd_data instead of lcd_putc means one function call less
+}
+
+void lcd_puts_p(PGM_P str)
+{
+	char tmp;
+
+	while ((tmp = pgm_read_byte(str)))
+	{
+		lcd_putc(tmp);
+		str++;
+	}
+}
+
+void lcd_gotoxy(unsigned char x, unsigned char y)
+{
+	if (y == 0)
+		lcd_command((1 << LCD_DDRAM) + LCD_START_LINE1 + x);
+	else
+		lcd_command((1 << LCD_DDRAM) + LCD_START_LINE2 + x);
+}
---
+++ firmware/shc_tea_maker/shc_tea_maker.c
@@ -0,0 +1,813 @@
+/*
+* This file is part of smarthomatic, http://www.smarthomatic.org.
+* Copyright (c) 2016 Uwe Freese
+*
+* smarthomatic is free software: you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the
+* Free Software Foundation, either version 3 of the License, or (at your
+* option) any later version.
+*
+* smarthomatic is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+* Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with smarthomatic. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <string.h>
+
+#include "rfm12.h"
+#include "../src_common/uart.h"
+#include "lcd.h"
+
+#include "../src_common/msggrp_generic.h"
+#include "../src_common/msggrp_gpio.h"
+
+#include "../src_common/e2p_hardware.h"
+#include "../src_common/e2p_generic.h"
+#include "../src_common/e2p_powerswitch.h"
+
+#include "onewire.h"
+
+#include "../src_common/aes256.h"
+#include "../src_common/util.h"
+#include "version.h"
+
+#include "../src_common/util_watchdog_init.h"
+
+#define SWITCH_COUNT 6 // Don't change! (PC0 to PC5 are supported)
+
+#define RELAIS_PORT PORTC // TODO: Configurable pins like in env sensor
+#define RELAIS_PIN_START 0
+
+#define BUTTON_DDR DDRB
+#define BUTTON_PORT PORTB
+#define BUTTON_PINPORT PINB
+#define BUTTON_UP_PIN    1
+#define BUTTON_DOWN_PIN  2
+#define BUTTON_LEFT_PIN  3
+#define BUTTON_RIGHT_PIN 4
+
+#define BUTTON_DEBOUNCE_COUNT 3
+#define BUTTON_DEBOUNCE_DELAY_MS 30
+
+// Power of RFM12B (since PCB rev 1.1) or RFM12 NRES (Reset) pin may be connected to PC3.
+// If not, only sw reset is used.
+#define RFM_RESET_PIN 3
+#define RFM_RESET_PORT_NR 1
+
+#define SEND_STATUS_EVERY_SEC 1800 // how often should a status be sent?
+#define SEND_VERSION_STATUS_CYCLE 50 // send version status x times less than switch status (~once per day)
+
+// PWM range around 1500ms. E.g. use 300 to use a range of 1.2..1.8 ms pulse. Don't use values above 500!
+#define PWM_RANGE 300
+
+uint16_t device_id;
+uint32_t station_packetcounter;
+bool switch_state[SWITCH_COUNT];
+uint16_t switch_timeout[SWITCH_COUNT];
+
+uint8_t rom_id[8]; // for 1-wire
+
+uint16_t send_status_timeout = 5;
+uint8_t version_status_cycle = SEND_VERSION_STATUS_CYCLE - 1; // send promptly after startup
+
+#define WINCH_PIN 1
+#define WINCH_DDR DDRB
+
+
+typedef enum
+{
+	MENU_CONFIRMATION,
+	MENU_BREWING_PRESET,
+	MENU_START_MODE_NOW,
+	MENU_START_MODE_TIMER,
+	MENU_START_MODE_COUNTDOWN,
+	MENU_WAIT_START_TIME
+} menu_pos_t;
+
+typedef enum
+{
+	BUTTON_UP,
+	BUTTON_DOWN,
+	BUTTON_RIGHT,
+	BUTTON_LEFT,
+	BUTTON_NONE
+} button_t;
+
+
+void io_init(void)
+{
+	// init I/O pins for buttons as input with pull-up active
+	cbi(BUTTON_DDR,  BUTTON_UP_PIN);
+	sbi(BUTTON_PORT, BUTTON_UP_PIN);
+	cbi(BUTTON_DDR,  BUTTON_DOWN_PIN);
+	sbi(BUTTON_PORT, BUTTON_DOWN_PIN);
+	cbi(BUTTON_DDR,  BUTTON_LEFT_PIN);
+	sbi(BUTTON_PORT, BUTTON_LEFT_PIN);
+	cbi(BUTTON_DDR,  BUTTON_RIGHT_PIN);
+	sbi(BUTTON_PORT, BUTTON_RIGHT_PIN);
+}
+
+// Timer1 (16 Bit) is used for the winch.
+// The servo drive needs a cycle of 20ms with a pulse of 1-2 ms, where 1,5ms means the
+// servo is standing still.
+void PWM_init(void)
+{
+	// Enable output pins
+	WINCH_DDR |= (1 << WINCH_PIN);
+
+	// OC1A: Fast PWM, 16 Bit, TOP = ICR1, non-inverting output
+	TCCR1A = (1 << WGM11) | (1 << COM1A1);
+	TCCR1B = (1 << WGM12) | (1 << WGM13) | (1 << CS10);
+
+	// count from 0 - 20000 = 20ms @ 1 MHz
+	ICR1 = 20000;
+	OCR1A = 1500; // stand still
+}
+
+// Use speed percentage in the range -100..100.
+void winch_speed(int8_t percentage)
+{
+	if (percentage > 100)
+	{
+		percentage = 100;
+	}
+	else if (percentage < -100)
+	{
+		percentage = -100;
+	}
+
+	OCR1A = 1500 + PWM_RANGE * (int16_t)percentage / 100;
+}
+
+// Accelerate from one speed to another one in the given amount of ms.
+// The speed (PWM value) is adjusted every 20ms, which is also the PWM speed of the servo.
+void winch_accelerate(int8_t startPercentage, int8_t endPercentage, uint16_t ms)
+{
+	int16_t steps = ms / 20;
+	int16_t speed;
+	
+	/*
+	UART_PUTF("startPercentage: %d\r\n", startPercentage);
+	UART_PUTF("endPercentage: %d\r\n", endPercentage);
+	UART_PUTF("ms: %d\r\n", ms);
+	UART_PUTF("steps: %d\r\n", steps); */
+	
+	for (uint8_t i = 0; i < steps; i++)
+	{
+		speed = startPercentage + ((int16_t)endPercentage - startPercentage) * i / steps;
+		winch_speed(speed);
+		_delay_ms(20);
+	}
+	
+	winch_speed(endPercentage);
+	UART_PUTF("end: %d\r\n", endPercentage);
+}
+
+void print_switch_state(void)
+{
+	uint8_t i;
+
+	for (i = 1; i <= SWITCH_COUNT; i++)
+	{
+		UART_PUTF("Switch %u ", i);
+		
+		if (switch_state[i - 1])
+		{
+			UART_PUTS("ON");
+		}
+		else
+		{
+			UART_PUTS("OFF");
+		}
+		
+		if (switch_timeout[i - 1])
+		{		
+			UART_PUTF(" (Timeout: %us)", switch_timeout[i - 1]);
+		}
+
+		UART_PUTS("\r\n");
+	}
+}
+
+void send_gpio_digitalporttimeout_status(void)
+{	
+	uint8_t i;
+	
+	UART_PUTS("Sending GPIO DigitalPortTimeout Status:\r\n");
+
+	print_switch_state();
+
+	inc_packetcounter();
+
+	// Set packet content
+	pkg_header_init_gpio_digitalporttimeout_status();
+	pkg_header_set_senderid(device_id);
+	pkg_header_set_packetcounter(packetcounter);
+	
+	for (i = 0; i < SWITCH_COUNT; i++)
+	{
+		msg_gpio_digitalporttimeout_set_on(i, switch_state[i]);
+		msg_gpio_digitalporttimeout_set_timeoutsec(i, switch_timeout[i]);
+	}
+
+	rfm12_send_bufx();
+}
+
+void send_deviceinfo_status(void)
+{
+	inc_packetcounter();
+
+	UART_PUTF("Send DeviceInfo: DeviceType %u,", DEVICETYPE_POWERSWITCH);
+	UART_PUTF4(" v%u.%u.%u (%08lx)\r\n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, VERSION_HASH);
+	
+	// Set packet content
+	pkg_header_init_generic_deviceinfo_status();
+	pkg_header_set_senderid(device_id);
+	pkg_header_set_packetcounter(packetcounter);
+	msg_generic_deviceinfo_set_devicetype(DEVICETYPE_POWERSWITCH);
+	msg_generic_deviceinfo_set_versionmajor(VERSION_MAJOR);
+	msg_generic_deviceinfo_set_versionminor(VERSION_MINOR);
+	msg_generic_deviceinfo_set_versionpatch(VERSION_PATCH);
+	msg_generic_deviceinfo_set_versionhash(VERSION_HASH);
+
+	rfm12_send_bufx();
+}
+
+void switchRelais(int8_t num, bool on, uint16_t timeout, bool dbgmsg)
+{
+	if (dbgmsg)
+	{
+		UART_PUTF3("Switching relais %u to %u with timeout %us.\r\n", num + 1, on, timeout);
+	}
+
+	if (num >= SWITCH_COUNT)
+	{
+		UART_PUTF("\r\nRelais number %u > SWITCH_COUNT, ignoring.", num);
+		return;
+	}
+
+	if (switch_state[num] != on)
+	{
+		switch_state[num] = on;
+		
+		if (on)
+		{
+			sbi(RELAIS_PORT, RELAIS_PIN_START + num);
+			switch_led(1);
+		}
+		else
+		{
+			cbi(RELAIS_PORT, RELAIS_PIN_START + num);
+			switch_led(0);
+		}
+	}
+	
+	if (e2p_powerswitch_get_switchstate(num) != on)
+	{
+		e2p_powerswitch_set_switchstate(num, on);
+	}
+	
+	if (switch_timeout[num] != timeout)
+	{
+		switch_timeout[num] = timeout;
+	}
+
+	if (e2p_powerswitch_get_switchtimeout(num) != timeout)
+	{
+		e2p_powerswitch_set_switchtimeout(num, timeout);
+	}
+}
+
+void process_gpio_digitalport(MessageTypeEnum messagetype)
+{
+	// "Set" or "SetGet" -> modify switch state
+	if ((messagetype == MESSAGETYPE_SET) || (messagetype == MESSAGETYPE_SETGET))
+	{
+		uint8_t i;
+		
+		// react on changed state (version for more than one switch...)
+		for (i = 0; i < SWITCH_COUNT; i++)
+		{
+			bool req_on = msg_gpio_digitalport_get_on(i);
+			UART_PUTF2("On[%u]:%u;", i, req_on);
+			switchRelais(i, req_on, 0, false);
+		}
+	}
+}
+
+void process_gpio_digitalpin(MessageTypeEnum messagetype)
+{
+	// "Set" or "SetGet" -> modify switch state
+	if ((messagetype == MESSAGETYPE_SET) || (messagetype == MESSAGETYPE_SETGET))
+	{
+		uint8_t req_pos = msg_gpio_digitalpin_get_pos();
+		bool req_on = msg_gpio_digitalpin_get_on();
+		UART_PUTF2("Pos:%u;On:%u;", req_pos, req_on);
+		switchRelais(req_pos, req_on, 0, false);
+	}
+}
+
+void process_gpio_digitalporttimeout(MessageTypeEnum messagetype)
+{
+	// "Set" or "SetGet" -> modify switch state
+	if ((messagetype == MESSAGETYPE_SET) || (messagetype == MESSAGETYPE_SETGET))
+	{
+		uint8_t i;
+		
+		// react on changed state (version for more than one switch...)
+		for (i = 0; i < SWITCH_COUNT; i++)
+		{
+			bool req_on = msg_gpio_digitalporttimeout_get_on(i);
+			uint16_t req_timeout = msg_gpio_digitalporttimeout_get_timeoutsec(i);
+
+			UART_PUTF2("On[%u]:%u;", i, req_on);
+			UART_PUTF2("TimeoutSec[%u]:%u;", i, req_timeout);
+
+			switchRelais(i, req_on, req_timeout, false);
+		}
+	}
+}
+
+void process_gpio_digitalpintimeout(MessageTypeEnum messagetype)
+{
+	// "Set" or "SetGet" -> modify switch state
+	if ((messagetype == MESSAGETYPE_SET) || (messagetype == MESSAGETYPE_SETGET))
+	{
+		uint8_t req_pos = msg_gpio_digitalpintimeout_get_pos();
+		bool req_on = msg_gpio_digitalpintimeout_get_on();
+		uint16_t req_timeout = msg_gpio_digitalpintimeout_get_timeoutsec();
+		UART_PUTF2("Pos:%u;On:%u;", req_pos, req_on);
+		UART_PUTF("TimeoutSec:%u;", req_timeout);
+		switchRelais(req_pos, req_on, req_timeout, false);
+	}
+}
+
+void send_ack(uint32_t acksenderid, uint32_t ackpacketcounter, bool error)
+{
+	// any message can be used as ack, because they are the same anyway
+	if (error)
+	{
+		UART_PUTS("Send error Ack\r\n");
+		pkg_header_init_gpio_digitalporttimeout_ack();
+	}
+
+	inc_packetcounter();
+	
+	// set common fields
+	pkg_header_set_senderid(device_id);
+	pkg_header_set_packetcounter(packetcounter);
+	
+	pkg_headerext_common_set_acksenderid(acksenderid);
+	pkg_headerext_common_set_ackpacketcounter(ackpacketcounter);
+	pkg_headerext_common_set_error(error);
+	
+	rfm12_send_bufx();
+}
+
+// Process a request to this device.
+// React accordingly on the MessageType, MessageGroup and MessageID
+// and send an Ack in any case. It may be an error ack if request is not supported.
+void process_request(MessageTypeEnum messagetype, uint32_t messagegroupid, uint32_t messageid)
+{
+	// remember some values before the packet buffer is destroyed
+	uint32_t acksenderid = pkg_header_get_senderid();
+	uint32_t ackpacketcounter = pkg_header_get_packetcounter();
+	
+	UART_PUTF("MessageGroupID:%u;", messagegroupid);
+	
+	if (messagegroupid != MESSAGEGROUP_GPIO)
+	{
+		UART_PUTS("\r\nERR: Unsupported MessageGroupID.\r\n");
+		send_ack(acksenderid, ackpacketcounter, true);
+		return;
+	}
+	
+	UART_PUTF("MessageID:%u;", messageid);
+
+	switch (messageid)
+	{
+		case MESSAGEID_GPIO_DIGITALPORT:
+			process_gpio_digitalport(messagetype);
+			break;
+		case MESSAGEID_GPIO_DIGITALPIN:
+			process_gpio_digitalpin(messagetype);
+			break;
+		case MESSAGEID_GPIO_DIGITALPORTTIMEOUT:
+			process_gpio_digitalporttimeout(messagetype);
+			break;
+		case MESSAGEID_GPIO_DIGITALPINTIMEOUT:
+			process_gpio_digitalpintimeout(messagetype);
+			break;
+		default:
+			UART_PUTS("\r\nERR: Unsupported MessageID.");
+			send_ack(acksenderid, ackpacketcounter, true);
+			return;
+	}
+	
+	UART_PUTS("\r\n");
+
+	// In all cases, use the digitalporttimer message as answer.
+	// It contains the data for *all* pins and *all* timer values.
+
+	// "Set" -> send "Ack"
+	if (messagetype == MESSAGETYPE_SET)
+	{
+		pkg_header_init_gpio_digitalporttimeout_ack();
+
+		UART_PUTS("Sending Ack\r\n");
+	}
+	// "Get" or "SetGet" -> send "AckStatus"
+	else
+	{
+		pkg_header_init_gpio_digitalporttimeout_ackstatus();
+		
+		uint8_t i;
+		
+		// react on changed state (version for more than one switch...)
+		for (i = 0; i < SWITCH_COUNT; i++)
+		{
+			// set message data
+			msg_gpio_digitalporttimeout_set_on(i, switch_state[i]);
+			msg_gpio_digitalporttimeout_set_timeoutsec(i, switch_timeout[i]);
+		}
+		
+		UART_PUTS("Sending AckStatus\r\n");
+	}
+
+	send_ack(acksenderid, ackpacketcounter, false);
+	send_status_timeout = 5;
+}
+
+// Check if incoming message is a legitimate request for this device.
+// If not, ignore it.
+void process_packet(uint8_t len)
+{
+	pkg_header_adjust_offset();
+
+	// check SenderID
+	uint32_t senderID = pkg_header_get_senderid();
+	UART_PUTF("Packet Data: SenderID:%u;", senderID);
+	
+	if (senderID != 0)
+	{
+		UART_PUTS("\r\nERR: Illegal SenderID.\r\n");
+		return;
+	}
+
+	// check PacketCounter
+	// TODO: Reject if packet counter lower than remembered!!
+	uint32_t packcnt = pkg_header_get_packetcounter();
+	UART_PUTF("PacketCounter:%lu;", packcnt);
+
+	if (0) // packcnt <= station_packetcounter ??
+	{
+		UART_PUTF("\r\nERR: Received PacketCounter < %lu.\r\n", station_packetcounter);
+		return;
+	}
+	
+	// write received counter
+	station_packetcounter = packcnt;
+	
+	e2p_powerswitch_set_basestationpacketcounter(station_packetcounter);
+	
+	// check MessageType
+	MessageTypeEnum messagetype = pkg_header_get_messagetype();
+	UART_PUTF("MessageType:%u;", messagetype);
+	
+	if ((messagetype != MESSAGETYPE_GET) && (messagetype != MESSAGETYPE_SET) && (messagetype != MESSAGETYPE_SETGET))
+	{
+		UART_PUTS("\r\nERR: Unsupported MessageType.\r\n");
+		return;
+	}
+	
+	// check device id
+	uint16_t rcv_id = pkg_headerext_common_get_receiverid();
+
+	UART_PUTF("ReceiverID:%u;", rcv_id);
+	
+	if (rcv_id != device_id)
+	{
+		UART_PUTS("\r\nWRN: DeviceID does not match.\r\n");
+		return;
+	}
+	
+	// check MessageGroup + MessageID
+	uint32_t messagegroupid = pkg_headerext_common_get_messagegroupid();
+	uint32_t messageid = pkg_headerext_common_get_messageid();
+	
+	process_request(messagetype, messagegroupid, messageid);
+}
+
+// Return currently pressed button, without using debouncing.
+// If more than one is pressed, prefer them in the following order:
+// UP, DOWN, LEFT, RIGHT
+button_t get_button(void)
+{
+	uint8_t state = BUTTON_PINPORT;
+	
+	if (0 == (state & (1 << BUTTON_UP_PIN)))
+	{
+		return BUTTON_UP;
+	}
+	else if (0 == (state & (1 << BUTTON_DOWN_PIN)))
+	{
+		return BUTTON_DOWN;
+	}
+	else if (0 == (state & (1 << BUTTON_LEFT_PIN)))
+	{
+		return BUTTON_LEFT;
+	}
+	else if (0 == (state & (1 << BUTTON_RIGHT_PIN)))
+	{
+		return BUTTON_RIGHT;
+	}
+	else
+	{
+		return BUTTON_NONE;
+	}
+}
+
+// wait until all buttons are released
+void button_wait_up(void)
+{
+	uint8_t count = 0;
+	
+	while (count < BUTTON_DEBOUNCE_COUNT)
+	{
+		_delay_ms(BUTTON_DEBOUNCE_DELAY_MS);
+		
+		if (get_button() == BUTTON_NONE)
+		{
+			count++;
+		}
+		else
+		{
+			count = 0;
+		}
+	}
+}
+
+// wait for button press and return which button was pressed
+button_t button_wait_down(void)
+{
+	uint8_t count = 0;
+	button_t last;
+	button_t current = BUTTON_NONE;
+	
+	while (count < BUTTON_DEBOUNCE_COUNT)
+	{
+		_delay_ms(BUTTON_DEBOUNCE_DELAY_MS);
+		
+		last = current;
+		current = get_button();
+		
+		if ((last == current) && (current != BUTTON_NONE))
+		{
+			count++;
+		}
+		else
+		{
+			count = 0;
+		}
+	}
+	
+	return current;
+}
+
+button_t button(void)
+{
+	button_wait_up();
+	return button_wait_down();
+}
+
+int main(void)
+{
+	uint8_t loop = 0;
+	uint16_t temperature;
+
+	// delay 1s to avoid further communication with uart or RFM12 when my programmer resets the MC after 500ms...
+	_delay_ms(700);
+
+	util_init();
+	/*
+	//check_eeprom_compatibility(DEVICETYPE_POWERSWITCH);
+
+	// read packetcounter, increase by cycle and write back
+	packetcounter = e2p_generic_get_packetcounter() + PACKET_COUNTER_WRITE_CYCLE;
+	e2p_generic_set_packetcounter(packetcounter);
+
+	// read last received station packetcounter
+	station_packetcounter = e2p_powerswitch_get_basestationpacketcounter();
+	
+	// read device id
+	device_id = e2p_generic_get_deviceid();
+
+	osccal_init();
+*/
+	uart_init();
+
+	UART_PUTS ("\r\n");
+	UART_PUTF4("smarthomatic Tea Maker v%u.%u.%u (%08lx)\r\n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, VERSION_HASH);
+	UART_PUTS("(c) 2016 Uwe Freese, www.smarthomatic.org\r\n");
+	osccal_info();
+	UART_PUTF ("DeviceID: %u\r\n", device_id);
+	UART_PUTF ("PacketCounter: %lu\r\n", packetcounter);
+	print_switch_state();
+	UART_PUTF ("Last received base station PacketCounter: %u\r\n\r\n", station_packetcounter);
+	
+	lcd_init();
+	
+	LCD_PUTS("--- TeaMaker ---");
+	lcd_gotoxy(0, 1);
+	LCD_PUTF4("%u.%u.%u %08lx", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, VERSION_HASH);
+
+	// init AES key
+	e2p_generic_get_aeskey(aes_key);
+	
+	//PWM_init();
+	
+	io_init();
+	
+	onewire_init();
+	
+	bool res = onewire_get_rom_id(rom_id);
+		
+	if (res) // error, no slave found
+	{
+		while (1)
+		{
+			led_blink(50, 50, 1);
+		}
+	}
+	
+	UART_PUTS("1-wire ROM ID: ");
+	print_bytearray(rom_id, 8);
+
+	/* // button and one wire temperature sensor check
+	while(1)
+	{
+		lcd_gotoxy(0, 0);
+		LCD_PUTF2("%u %u", 0b00011110 & BUTTON_PINPORT, get_button());
+		lcd_gotoxy(0, 1);
+		temperature = onewire_get_temperature(rom_id);
+		LCD_PUTF2("Temp: %u.%02u degC", temperature / 100, temperature % 100);
+		_delay_ms(100);
+	} */
+	
+	menu_pos_t menu_pos = MENU_CONFIRMATION;
+	uint8_t brewing_preset_pos = 0;
+	uint8_t brewing_preset_count = 5;
+	uint8_t timer_pos = 0;
+	uint8_t timer_count = 5;
+	uint8_t countdown_pos = 0;
+	uint8_t countdown_count = 5;
+	
+	while (1)
+	{
+		switch (menu_pos)
+		{
+			case MENU_CONFIRMATION:
+				lcd_clear();
+				LCD_PUTS("Fill in üöäßÄÖÜ");
+				lcd_gotoxy(0,1);
+				LCD_PUTS("Water & Tea!");
+				
+				switch (button())
+				{
+					case BUTTON_RIGHT:
+						menu_pos = MENU_BREWING_PRESET;
+						break;
+					default:
+						break;
+				}
+
+				break;
+
+			case MENU_BREWING_PRESET: 
+				lcd_clear();
+				LCD_PUTF("Preset %d", brewing_preset_pos + 1); // user perceived number counts from 1 on
+				
+				switch (button())
+				{
+					case BUTTON_UP:
+						brewing_preset_pos = (brewing_preset_pos + brewing_preset_count - 1) % brewing_preset_count;
+						break;
+					case BUTTON_DOWN:
+						brewing_preset_pos = (brewing_preset_pos + 1) % brewing_preset_count;
+						break;
+					case BUTTON_RIGHT:
+						menu_pos = MENU_START_MODE_NOW;
+						break;
+					case BUTTON_LEFT:
+						menu_pos = MENU_CONFIRMATION;
+						break;
+					default:
+						break;
+				}
+				
+				break;
+			
+			case MENU_START_MODE_NOW:
+				lcd_clear();
+				LCD_PUTS("Start now");
+				
+				switch (button())
+				{
+					case BUTTON_UP:
+						menu_pos = MENU_START_MODE_TIMER;
+						break;
+					case BUTTON_DOWN:
+						menu_pos = MENU_START_MODE_COUNTDOWN;
+						break;
+					case BUTTON_RIGHT:
+						menu_pos = MENU_WAIT_START_TIME;
+						break;
+					case BUTTON_LEFT:
+						menu_pos = MENU_BREWING_PRESET;
+						break;
+					default:
+						break;
+				}
+				
+				break;
+				
+			case MENU_START_MODE_TIMER:
+				lcd_clear();
+				LCD_PUTF("Timer %d", timer_pos);
+				
+				switch (button())
+				{
+					case BUTTON_UP:
+						break;
+					case BUTTON_DOWN:
+						break;
+					case BUTTON_RIGHT:
+						break;
+					case BUTTON_LEFT:
+						menu_pos = MENU_BREWING_PRESET;
+						break;
+					default:
+						break;
+				}
+
+				break;
+				
+			case MENU_START_MODE_COUNTDOWN:
+				lcd_clear();
+				LCD_PUTF("Countdown %d", countdown_pos);
+				
+				switch (button())
+				{
+					case BUTTON_UP:
+						break;
+					case BUTTON_DOWN:
+						break;
+					case BUTTON_RIGHT:
+						break;
+					case BUTTON_LEFT:
+						menu_pos = MENU_BREWING_PRESET;
+						break;
+					default:
+						break;
+				}
+
+				break;
+				
+			case MENU_WAIT_START_TIME:
+				lcd_clear();
+				LCD_PUTS("Wait for stime");
+				_delay_ms(5000);
+				menu_pos = MENU_CONFIRMATION;
+				
+				break;
+		}
+	}
+	
+	
+	
+	/* // winch test
+	while (42)
+	{
+		winch_accelerate(0, 20, 500);
+		winch_accelerate(20, 100, 500);
+		_delay_ms(500);
+		winch_accelerate(100, 15, 1000);
+		_delay_ms(500);
+		winch_accelerate(15, 0, 500);
+		_delay_ms(3000);
+	} */
+	
+
+//	rfm_watchdog_init(device_id, e2p_powerswitch_get_transceiverwatchdogtimeout(), RFM_RESET_PORT_NR, RFM_RESET_PIN);
+//	rfm12_init();
+
+//	sei();
+
+}
